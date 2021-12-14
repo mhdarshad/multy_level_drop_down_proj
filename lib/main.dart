d465d596e3737e0a74e0f92d39f9476f4e26c6b9
mhdarshad <38720792+mhdarshad@users.noreply.github.com>
2021-12-14 21:47:53 +0530

    fianl

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'modle/cars.dart';
@@ -10,21 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,62 +25,150 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key, @required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _future;
-
-  Widget _cars =const DropdownMenuItem<Widget>(child: Text("Nissan"),);
-
-
+  List<CarBrand> _carbrandlist=[];
+  String _brandvalue;
+  String _carvalue;
+  List<String> _carlist =[];
+  Future<String> _getCarList() async {
+    cartrepo.readJson().then((value) {
+      setState(() {
+        _carlist = [...value.cars.toJson().keys];
+      });
+    });
+  }
 
 @override
   void initState() {
     // TODO: implement initState
- setState(() {
-   _future = cartrepo.readJson();
- });
+  _getCarList();
     super.initState();
   }
-
+  Future<CarBrand> _getBrandsList(String newValue) async {
+    List<CarBrand> brand = [];
+    for (var element in _carlist) {
+      if(element == newValue){
+        cartrepo.readJson().then((value) {
+          value.cars.toJson()[newValue].asMap().forEach((index, value){
+            brand.add(CarBrand.fromJson(value));
+          });
+          _carbrandlist.clear();
+          setState(() {
+            _brandvalue=null;
+            _carvalue = newValue;
+            _carbrandlist = brand;
+          });
+          print(_carvalue);
+        });
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child:FutureBuilder<Carss>(future: _future,
-          builder:(context,AsyncSnapshot<Carss> snapshot){
-          // snapshot.hasData
-          final itemlist = [const DropdownMenuItem<Widget>(child: Text("Nissan"),),const DropdownMenuItem<Widget>(child: Text("Nissan"),),];
-          return DropdownButtonFormField<Widget>(
-            isDense: true,
-            items: itemlist.toList(),
-            onChanged: (Widget cars){
-              setState(() {
-                _cars = cars;
-              });
-            },
-            value: _cars,
-          );
-      },
-          ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child:  ListView(
+            padding: EdgeInsets.all(20.0),
+            children: [
+
+              Container(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: DropdownButtonHideUnderline(
+                        child: ButtonTheme(
+                          alignedDropdown: true,
+                          child: DropdownButton<String>(
+                            value: _carvalue,
+                            iconSize: 30,
+                            icon: (null),
+                            style: const TextStyle(
+                              color: Colors.black54,
+                              fontSize: 16,
+                            ),
+                            hint: Text('Select Cars'),
+                            onChanged: (String newValue) =>_getBrandsList(newValue),
+                            items: _carlist?.map((item) {
+                              return DropdownMenuItem(
+                                child: Text(item),
+                                value: item.toString(),
+                              );
+                            })?.toList() ?? [],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+
+              Container(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: DropdownButtonHideUnderline(
+                        child: ButtonTheme(
+                          alignedDropdown: true,
+                          child: DropdownButton<String>(
+                            value: _brandvalue,
+                            iconSize: 30,
+                            icon: (null),
+                            style: const TextStyle(
+                              color: Colors.black54,
+                              fontSize: 16,
+                            ),
+                            hint: Text('Select City'),
+                            onChanged: (String newValue) {
+                              setState(() {
+                                _brandvalue = newValue;
+                                print(_brandvalue);
+                              });
+                            },
+                            items: _carbrandlist?.map((item) {
+                              return DropdownMenuItem(
+                                child: Text("${item.model} door:${item.doors}"),
+                                value: item.model.toString(),
+                              );
+                            })?.toList() ?? [],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Text("${_carvalue??""} , ${_brandvalue??""}",style: const TextStyle(fontSize: 20),),
+                ],
+              )
+            ],)
+        ),
       ),
     );
   }
+
 }
